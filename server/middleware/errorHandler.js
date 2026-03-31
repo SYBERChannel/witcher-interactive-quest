d59bf0b6f685c5bf9b6e090f95d98c4b1049d61b
SYBER <valerik.com.is@gmail.com>
2026-03-31 20:23:24 +0300

    debug

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,10 +1,26 @@
-const errorHandler = (err, req, res, next) => {
+/*const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.isOperational ? err.message : "Internal server error";
 
     if (process.env.NODE_ENV === "development") {
         console.error(err);
     }
+
+    res.status(statusCode).json({
+        status: "error",
+        statusCode,
+        message,
+    });
+};
+
+module.exports = errorHandler;*/
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+
+    const message = err.message || "Internal server error";
+
+    console.error("FULL ERROR FOR DEBUG:", err);
 
     res.status(statusCode).json({
         status: "error",
